696243ac88a306b6d3db162a77c7e5e7cdf51236
zhengKyles <zhengkyles@gmail.com>
2023-10-30 10:15:00 +0800

    Update WpPermission.kt

--- permission/src/main/java/com/wp/permission/WpPermission.kt
+++ permission/src/main/java/com/wp/permission/WpPermission.kt
@@ -16,5 +16,10 @@ enum class WpPermission(var permission: String) {
     READ_PHONE_STATE(Manifest.permission.READ_PHONE_STATE),
     CAMERA(Manifest.permission.CAMERA),
     CALL_PHONE(Manifest.permission.CALL_PHONE),
-    SYSTEM_ALERT_WINDOW(Manifest.permission.SYSTEM_ALERT_WINDOW)
+    SYSTEM_ALERT_WINDOW(Manifest.permission.SYSTEM_ALERT_WINDOW),
+        BLUETOOTH_CONNECT(Manifest.permission.BLUETOOTH_CONNECT),
+    BLUETOOTH_SCAN(Manifest.permission.BLUETOOTH_SCAN),
+    BLUETOOTH_ADVERTISE(Manifest.permission.BLUETOOTH_ADVERTISE),
+    BLUETOOTH_PRIVILEGED(Manifest.permission.BLUETOOTH_PRIVILEGED),
+    BLUETOOTH_ADMIN(Manifest.permission.BLUETOOTH_ADMIN),
 }
